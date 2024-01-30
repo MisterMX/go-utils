d0278e405414adab7f89d9453791f0b350688969
MisterMX <mbxd12@web.de>
2024-01-30 14:51:39 +0100

    feat(k8s/yaml): Add file unmarshallers
    Signed-off-by: MisterMX <mbxd12@web.de>

--- k8s/yaml/unmarshal.go
+++ k8s/yaml/unmarshal.go
@@ -4,11 +4,21 @@ import (
 	"bufio"
 	"bytes"
 	"io"
+	"io/fs"
 	"unicode"
 
 	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/util/yaml"
 )
+
+// UnmarshalFile reads a single object from a YAML file.
+func UnmarshalFile(fsys fs.FS, filename string, target any) error {
+	raw, err := fs.ReadFile(fsys, filename)
+	if err != nil {
+		return err
+	}
+	return yaml.Unmarshal(raw, target)
+}
 
 // UnmarshalObjects parses all objects from a multi-document YAML stream.
 // Documents that are empty (including no comments) or contain only white space
@@ -16,6 +26,16 @@ import (
 func UnmarshalObjects[T any](rawYAML []byte) ([]T, error) {
 	buf := bytes.NewBuffer(rawYAML)
 	return UnmarshalObjectsReader[T](buf)
+}
+
+// UnmarshalObjectsFile call [UnmarshalObjectsReader] with a file stream.
+func UnmarshalObjectsFile[T any](fsys fs.FS, filename string) ([]T, error) {
+	file, err := fsys.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return UnmarshalObjectsReader[T](file)
 }
 
 // UnmarshalObjectsReader parses all objects from a multi-document YAML stream.
